caa7cc0dd1d38c4be0f98909df5ab0558b6fff53
InsiaMurtaza <165552743+InsiaMurtaza@users.noreply.github.com>
2024-08-26 13:09:08 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,124 @@
+import inquirer from "inquirer";
+
+interface BankAccount {
+    accountNumber:number;
+    accountBalance:number;
+    checkBalance():void;
+    deposit(amount:number):void;
+    withdraw(amount:number):void
+}
+class BankAccount implements BankAccount {
+    accountNumber:number;
+    accountBalance:number;
+    constructor(accountNumber:number,accountBalance:number){
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance
+    }
+    deposit(amount:number){
+        if(amount > 100){
+            amount-= 1;
+        }
+    this.accountBalance += amount;
+    console.log(`$${amount} has been deposited in ${this.accountNumber}\nNew Balance is $${this.accountBalance}`);
+    }
+    withdraw(amount:number){
+        if(this.accountBalance>=amount){
+        this.accountBalance-= amount;
+        console.log(`You have successfully withdrew $${amount}...\nYour remaining balance is $${this.accountBalance}`);
+        }
+        else {
+        console.log("You have Insufficient Balance");
+        }
+    }
+    checkBalance(){
+        console.log(`Your account balance is $${this.accountBalance}`);
+    }
+}
+class Customer {
+    firstName:string;
+    lastName:string;
+    gender:string;
+    age:number;
+    mobileNumber:number;
+    accountNumber:BankAccount;
+    constructor(firstName:string,lastName:string,gender:string,age:number,mobileNumber:number,accountNumber:BankAccount){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.accountNumber = accountNumber;
+    }
+}
+const bankaccounts:BankAccount[]=[
+    new BankAccount(1009435670,2000),
+    new BankAccount(1001453983,1000),
+    new BankAccount(1009475369,500)
+]   
+
+const customers:Customer[]=[
+    new Customer("Insia","Murtaza","female",42,3343437271,bankaccounts[0]),
+    new Customer("Kashif","Faisal","male",45,3135444853,bankaccounts[1]),
+    new Customer("Shehzad","Farooqi","Male",55,3457862543,bankaccounts[2])
+]
+
+        const askCustomer = await inquirer.prompt([{
+            name:"accountnumber",
+            message:"Enter Your Account Number: ",
+            type:"number"
+        }])
+    const matchaccountnumber = customers.find(customer=>customer.accountNumber.accountNumber === askCustomer.accountnumber)
+    
+    do{
+
+    if(matchaccountnumber){
+        console.log(`Welcome ${matchaccountnumber.firstName+ " " + matchaccountnumber.lastName}`);
+        
+        let operation = await inquirer.prompt([{
+            name:"select",
+            message:"What would you like to do?",
+            type:"list",
+            choices:["Balance Inquiry","Deposit","Withdraw","Exit"]
+        }])
+        switch(operation.select){
+            case "Balance Inquiry":
+                console.log(`Your account balance is $${matchaccountnumber.accountNumber.accountBalance}`);
+            break;
+            case "Deposit":
+                let asktoDeposit = await inquirer.prompt([{
+                    name:"amount",
+                    message:"Enter the amount you want to deposit: ",
+                    type:"number"
+                }])
+                 matchaccountnumber.accountNumber.deposit(asktoDeposit.amount);     
+            break;
+            case "Withdraw":
+                let asktoWithdraw = await inquirer.prompt([{
+                    name:"amount",
+                    message:"Enter the amount you want to withdraw: ",
+                    type:"number"
+                }])
+                matchaccountnumber.accountNumber.withdraw(asktoWithdraw.amount);
+            break;
+            case "Exit":
+            console.log("Thankyou for using our bank service!");
+            process.exit();
+        }
+        let askagain = await inquirer.prompt([{
+            name:"tomaketransaction",
+            message:"Do you want to make another transaction? ",
+            type:"list",
+            choices:["Yes","No"]
+        
+        }])
+        if(askagain.tomaketransaction == "No"){
+        console.log("Thankyou for using our bank service!");
+        process.exit();
+        }
+        }    
+    else {
+        console.log("Invalid Account Number...");
+        process.exit();
+    } 
+    }
+        while(true);
